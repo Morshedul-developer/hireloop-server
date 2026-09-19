75365cee9c6cdcd7bdf94acadfb7f64ae0c8bad8
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-09-19 09:43:25 +0600

    added time to all the post api(s)

--- index.js
+++ index.js
@@ -56,7 +56,11 @@ async function run() {
 
     app.post("/api/jobs", async (req, res) => {
         const job = req.body;
-        const result = await jobCollection.insertOne(job);
+        const newJob = {
+          ...job,
+          createdAt: new Date(),
+        }
+        const result = await jobCollection.insertOne(newJob);
         res.send(result);
     })
 
@@ -73,7 +77,11 @@ async function run() {
 
     app.post("/api/companies", async (req, res) => {
         const company = req.body;
-        const result = await companyCollection.insertOne(company);
+        const newCompany = {
+          ...company,
+          createdAt: new Date(),
+        };
+        const result = await companyCollection.insertOne(newCompany);
         res.send(result);
     });
 
